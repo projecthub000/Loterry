6d990aa6192c2d092a5dba8b134dfcbe01d86e63
Aditya Raj <76516486+projecthub000@users.noreply.github.com>
2023-02-18 17:12:53 +0530

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -1,31 +1,35 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 contract lottery{
-    address payable[] public participants;
-    address public manager;
-    constructor(){
-        manager=msg.sender;
+    address payable[] public users; // making dynamic array which is of address type
+
+    address public manager; // command is in manager's hand
+
+    constructor(){ // it's up to us to create it or not
+        manager=msg.sender; //manager is now the address with which our contract is deployed 
+
     }
-    receive() external payable{
-        require(msg.value==2 ether);
-        participants.push(payable(msg.sender));
+
+    receive() external payable{ // only one for the contract which is of payable type which takes ether from participants.
+        require(msg.value>=2 ether); // participants must satisfy this condition
+        users.push(payable(msg.sender)); 
     }
-    function getbalance() public view returns(uint){
-        require(msg.sender==manager);
+    function getbalance() public view returns(uint){ 
+        require(msg.sender==manager); // only manger can have power to access this function
         return address(this).balance;
     }
     function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,users.length))); // randomly generated hash and later on converted into int type
     }
-    function selectwinner() public{
-        require(msg.sender==manager);
-        require(participants.length>=3);
-        uint r=random();
-        uint index=r%participants.length;
+    function selectwinner() public{ 
+        require(msg.sender==manager); //only manger can have power to access this function
+        require(users.length>=3); 
+        uint r=random(); //calling random function in 'r' variable
+        uint index=r%users.length; // just like of hash function
         address payable winner;
-        winner=participants[index];
-        winner.transfer(getbalance());
-        participants=new address payable[](0);
+        winner=users[index];
+        winner.transfer(getbalance()); // transfering all balanace of getbalance function in winner address which is one of the participants
+        users=new address payable[](0); // reseting users
     }
 }
